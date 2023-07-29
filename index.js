d8e98bde47db7f1281146109e229a7cacce66fc9
ecemetin <eceyurdusevimli@gmail.com>
2023-07-29 17:37:01 +0300

    increase decrease save and reset functionalities are implemented

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 
 function increment() {
-
+    count += 1;
+    countEl.textContent = count;
+}
+function decrement() {
+    count -= 1;
+    countEl.textContent = count;
 }
 function save() {
+    let previousEntries = " "+ count + " - "
+    saveEl.textContent += previousEntries;
 
 }
 function reset() {
-
+    countEl.textContent = 0;
+    saveEl.textContent = 0;
 }
