c2cc4708813836dda93e6833a0ad273ceaea22ae
ecemetin <eceyurdusevimli@gmail.com>
2023-07-29 16:45:57 +0300

    decrement botton added

---
+++ index.js
@@ -0,0 +1,13 @@
+let count = 0;
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
+function increment() {
+
+}
+function save() {
+
+}
+function reset() {
+
+}
